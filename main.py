0c91c1c362716069f782f8cd3f86b3a45925b383
Maxim <maximnoritsyn@gmail.com>
2022-02-20 13:09:41 +0200

    create storage

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.storage.jsonstore import JsonStore
 from kivy.uix.button import Button
 
-
+store = JsonStore('PTAnamnes_Store.json')
 
 # Declare both screens
 class LoginScreen(Screen):
@@ -13,11 +14,14 @@ class MainScreen(Screen):
     pass
 
 class PTApp(App):
+
     def build(self):
+
         sm = ScreenManager()
-        sm.add_widget(LoginScreen(name="login"))
-        sm.add_widget(MainScreen(name="main"))
-        sm.current = 'login'
+        if store.exists('LoggedUser'):
+            sm.add_widget(MainScreen(name="main"))
+        else:
+            sm.add_widget(LoginScreen(name="login"))
         return sm
 
 
