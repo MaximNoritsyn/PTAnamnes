8ac804ecab568e543b32ff6a52277f4df3bc6fcc
Maxim <maximnoritsyn@gmail.com>
2022-02-23 22:02:56 +0200

    login screen with poput dont work at all

--- main.py
+++ main.py
@@ -1,26 +1,40 @@
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.popup import Popup
+from kivy.uix.screenmanager import Screen
 from kivy.storage.jsonstore import JsonStore
-from kivy.properties import BooleanProperty
 
 store_user = JsonStore('PTA_UserData.json')
 
 
-class LoginScreen(Screen):
-
-    is_logged = BooleanProperty(store_user.exists('User'))
+class LoginPopup(Popup):
 
     def pt_login(self):
-        if self.ids['txt_input'].text != '':
-            store_user.put('User', name=self.ids['txt_input'].text)
+        if self.ids['login'].text != '':
+            store_user.put('User', name=self.ids['login'].text)
+            self.dismiss()
         else:
             # we need say to user
             pass
 
 
+def show_login_window():
+    content = LoginPopup()
+
+    window = Popup(title="Log in first", content=content, auto_dismiss=False)
+    window.open()
+
+
+def is_not_logged():
+    return not store_user.exists('User')
+
+
 class MainScreen(Screen):
-    pass
+
+    def __init__(self, **kwargs):
+        super(MainScreen, self).__init__(**kwargs)
+        if is_not_logged():
+            show_login_window()
 
 
 class SettingsScreen(Screen):
@@ -29,8 +43,7 @@ class SettingsScreen(Screen):
 
 class PTApp(App):
     def build(self):
-        # Retornar com função Builder.load_string evita erros em acentuação.
-        return Builder.load_string(open('PT.kv', encoding='UTF-8').read())
+        pass
 
 
 if __name__ == '__main__':
