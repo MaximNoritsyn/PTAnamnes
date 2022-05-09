3c8d861009d0f3a994a647bde10805931e1f61a6
Maxim <maximnoritsyn@gmail.com>
2022-05-09 21:27:55 +0300

    popup window - trash. screen submit for the first lunch

--- main.py
+++ main.py
@@ -1,56 +1,44 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.popup import Popup
-from kivy.uix.screenmanager import Screen
+from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
 from kivy.storage.jsonstore import JsonStore
 from kivy.core.window import Window
 
 store_user = JsonStore('PTA_UserData.json')
 
 
-class LoginPopup(Popup):
+def is_not_logged():
+    return not store_user.exists('User')
 
-    def __init__(self):
-        super().__init__()
-        self.pop = Popup(title="", content=self, auto_dismiss=False)
 
-    def open_login(self):
-        self.pop.open()
+class MainScreen(Screen):
+    pass
 
+
+class SubmitScreen(Screen):
     def pt_login(self):
         if self.ids['login'].text != '':
             store_user.put('User', name=self.ids['login'].text)
-            self.pop.dismiss()
+            sm.current = 'main'
         else:
             # we need say to user
             pass
 
 
-def is_not_logged():
-    return not store_user.exists('User')
+Builder.load_file("PT.kv")
 
 
-def show_login_window():
-    content = LoginPopup()
-    content.open_login()
-
-
-class MainScreen(Screen):
-
-    def __init__(self, **kwargs):
-        super(MainScreen, self).__init__(**kwargs)
-        if is_not_logged():
-            show_login_window()
-
-
-class SettingsScreen(Screen):
-    pass
+sm = ScreenManager()
+sm.add_widget(MainScreen(name='main'))
+sm.add_widget(SubmitScreen(name='submit'))
 
 
 class PTApp(App):
     def build(self):
-        return Builder.load_file("PT.kv")
-
+        if is_not_logged():
+            sm.current = 'submit'
+        return sm
 
 if __name__ == '__main__':
     app = PTApp()
